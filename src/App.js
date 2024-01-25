63fa922bb8a65be7e1b27864280b13939f91136a
annurtasnimislam <annurtasnimislam@gmail.com>
2024-01-25 15:49:17 +0600

    fix: option box design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import { useState } from "react";
-import "./App.css";
+import classes from "./App.module.css";
 import DragBox from "./components/DragBox";
 
 export default function App() {
-  const [selectedOption, setSelectedOption] = useState("top");
+  const [toolPos, setToolPos] = useState("top");
 
   return (
-    <div className="app">
-      <select
-        value={selectedOption}
-        onChange={(e) => setSelectedOption(e.target.value)}
-      >
-        <option value={"left"}>left</option>
-        <option value={"right"}>right</option>
-        <option value={"top"}>top</option>
-        <option value={"bottom"}>bottom</option>
-      </select>
-
-      <DragBox toolTipPos={selectedOption} />
+    <div className={classes.wrapper}>
+      <div className={classes.container}>
+        <p>Position of Tooltip:</p>
+        <select
+          className={classes.option}
+          value={toolPos}
+          onChange={(e) => setToolPos(e.target.value)}
+        >
+          <option value={"left"}>Left</option>
+          <option value={"right"}>Right</option>
+          <option value={"top"}>Top</option>
+          <option value={"bottom"}>Bottom</option>
+        </select>
+      </div>
+      <DragBox toolTipPos={toolPos} />
     </div>
   );
 }
--- src/components/DragBox.jsx
+++ src/components/DragBox.jsx
@@ -10,7 +10,7 @@ const Container = ({ toolTipPos }) => {
   const [pos, setPos] = useState({ x: 250, y: 250 });
   const [rel, setRel] = useState({ x: null, y: null });
 
-  const [containerPos, setContainerPos] = useState({ x: 420, y: 80 });
+  const [containerPos, setContainerPos] = useState({ x: 420, y: 100 });
   const [containerRel, setContainerRel] = useState({ x: null, y: null });
 
   const containerRef = useRef(null);
